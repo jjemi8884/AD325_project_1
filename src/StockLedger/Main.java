717dce41981cd81aaa14c3d9beb28d3babbd6fad
Justin Jemison <jjemi8884@gmail.com>
2023-10-18 20:13:28 -0700

    got to printing

--- src/StockLedger/Main.java
+++ src/StockLedger/Main.java
@@ -6,11 +6,13 @@ public class Main {
     public static void main(String[] args) throws EmptyQueueException {
         StockLedger ledger = new StockLedger();
         ledger.buy("AARP", 20, 45.00);
-        ledger.buy("ESP", 50, 35.00);
-        ledger.buy("hp", 100, 25.00);
-        ledger.buy("AARP", 30, 45.00);
-        ledger.buy("AARP", 40, 35.00);
+        ledger.buy("AAPL", 50, 75.00);
+        ledger.buy("MSFT",20, 95.00);
         System.out.println(ledger);
-
+        //ledger.sell("AARP", 30, 65.00);
+        System.out.println(ledger);
+        //ledger.sell("AAPL", 10, 65.00);
+        System.out.println(ledger);
+        ledger.buy
     }
 }
